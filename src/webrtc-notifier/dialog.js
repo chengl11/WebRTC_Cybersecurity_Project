973bd25a4e330d6039742d7571e2ea483efdd337
Lin Cheng <44706693+chengl11@users.noreply.github.com>
2020-11-29 10:06:03 -0500

    add dialog style sheet

--- src/webrtc-notifier/dialog.js
+++ src/webrtc-notifier/dialog.js
@@ -1,12 +1,22 @@
-function changePosition() {
-    var screenAvailWidth = window.screen.availWidth;
-    var screenAvailHeight = window.screen.availHeight;
-    var iHeight = 400;
-    var iWidth = 400;
-    var iTop = (screenAvailHeight - iHeight) / 2;
-    var iLeft = (screenAvailWidth - iWidth) / 2;
 
-    window.resizeTo(iHeight,iWidth);
-    window.moveTo(iLeft,iTop);
+var continueButton = window.document.getElementById("continue-button");
+
+continueButton.addEventListener("click", closeWin);
+
+function closeWin() {
+    window.close();
 }
-changePosition();
+
+// function changePosition() {
+//     var screenAvailWidth = window.screen.availWidth;
+//     var screenAvailHeight = window.screen.availHeight;
+//     var iHeight = 400;
+//     var iWidth = 400;
+//     var iTop = (screenAvailHeight - iHeight) / 2;
+//     var iLeft = (screenAvailWidth - iWidth) / 2;
+
+//     window.resizeTo(iHeight,iWidth);
+//     window.moveTo(iLeft,iTop);
+// }
+// changePosition();
+
